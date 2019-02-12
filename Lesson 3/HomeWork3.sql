36234d736e40ad632a7482b2ce3a68a414c30805
Prokopchuk Sergey <pro5erg.gkhk@gmail.com>
2019-02-12 08:19:06 +0200

    First commit.

---
+++ Lesson 3/HomeWork3.sql
@@ -0,0 +1,46 @@
+﻿use WideWorldImporters;
+
+go
+
+/*
+1. Довставлять в базу 5 записей используя insert в таблицу Customers или Suppliers
+2. удалите 1 запись из Customers, которая была вами добавлена
+3. изменить одну запись, из добавленных через UPDATE
+4. Написать MERGE, который вставит вставит запись в клиенты, если ее там нет, и изменит если она уже есть
+5. Напишите запрос, который выгрузит данные через bcp out и загрузить через bulk insert
+*/
+
+insert into [Sales].[Customers](
+	[CustomerName]
+      ,[BillToCustomerID]
+      ,[CustomerCategoryID]
+      ,[BuyingGroupID]
+      ,[PrimaryContactPersonID]
+      ,[AlternateContactPersonID]
+      ,[DeliveryMethodID]
+      ,[DeliveryCityID]
+      ,[PostalCityID]
+      ,[CreditLimit]
+      ,[AccountOpenedDate]
+      ,[StandardDiscountPercentage]
+      ,[IsStatementSent]
+      ,[IsOnCreditHold]
+      ,[PaymentDays]
+      ,[PhoneNumber]
+      ,[FaxNumber]
+      ,[DeliveryRun]
+      ,[RunPosition]
+      ,[WebsiteURL]
+      ,[DeliveryAddressLine1]
+      ,[DeliveryAddressLine2]
+      ,[DeliveryPostalCode]
+      ,[DeliveryLocation]
+      ,[PostalAddressLine1]
+      ,[PostalAddressLine2]
+      ,[PostalPostalCode]
+      ,[LastEditedBy]
+      ,[ValidFrom]
+      ,[ValidTo]
+)
+values
+();
