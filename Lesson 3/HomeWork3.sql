5a71788e15df64357e3c497d8a9597d2d4adf929
Prokopchuk Sergey <pro5erg.gkhk@gmail.com>
2019-02-13 08:34:11 +0200

    Lesson 3, Task 2. Delete 1 customer.

--- Lesson 3/HomeWork3.sql
+++ Lesson 3/HomeWork3.sql
@@ -40,3 +40,8 @@ values
 ('CustomerName30', 1, 3, 1, 1011, 1012, 3, 20672, 20672, '2013-01-01', 0.000, 0, 0, 7, '(239) 555-0199', '(239) 555-0199', 'https://example.com/CustomerName1', 'DeliveryAddressLine1', 'Code1', 'PostalAddressLine1', 'Code1', 1),
 ('CustomerName40', 1, 3, 1, 1011, 1012, 3, 20672, 20672, '2013-01-01', 0.000, 0, 0, 7, '(239) 555-0199', '(239) 555-0199', 'https://example.com/CustomerName1', 'DeliveryAddressLine1', 'Code1', 'PostalAddressLine1', 'Code1', 1),
 ('CustomerName50', 1, 3, 1, 1011, 1012, 3, 20672, 20672, '2013-01-01', 0.000, 0, 0, 7, '(239) 555-0199', '(239) 555-0199', 'https://example.com/CustomerName1', 'DeliveryAddressLine1', 'Code1', 'PostalAddressLine1', 'Code1', 1);
+
+go 
+
+delete from [Sales].[Customers]
+where [CustomerName] = 'CustomerName50'
