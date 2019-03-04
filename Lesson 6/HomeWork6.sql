67a044d7dfdae986b440f26c63185a8ca6617f71
Prokopchuk Sergey <pro5erg.gkhk@gmail.com>
2019-03-04 08:37:55 +0200

    Lesson 6. Made small fixes due to feedback.

--- Lesson 6/HomeWork6.sql
+++ Lesson 6/HomeWork6.sql
@@ -28,27 +28,21 @@ go
 
 set statistics time on;
 
-Print('Query with Windows Function')
+print('Query with Windows Function')
 
 select 
 	IL.InvoiceID,
 	I.InvoiceDate,
 	C.CustomerName,
 	IL.Quantity*IL.UnitPrice as [Sale Amount],
-	SUM(IL.Quantity*IL.UnitPrice) over(partition by year(I.InvoiceDate) order by month(I.InvoiceDate)) as [Sale Amount by Month]
-from [Sales].[InvoiceLines] as IL
-join [Sales].[Invoices] as I
-on I.InvoiceID = IL.InvoiceID
-join [Sales].[Customers] as C
-on C.CustomerID = I.CustomerID
+	sum(IL.Quantity*IL.UnitPrice) over(partition by year(I.InvoiceDate) order by month(I.InvoiceDate)) as [Sale Amount by Month]
+from [Sales].[InvoiceLines] as IL 
+join [Sales].[Invoices] as I on I.InvoiceID = IL.InvoiceID
+join [Sales].[Customers] as C on C.CustomerID = I.CustomerID
 where I.InvoiceDate  >= '2015-01-01'
-order by I.InvoiceDate, IL.Quantity*IL.UnitPrice, IL.InvoiceID, C.CustomerName
+order by I.InvoiceDate, [Sale Amount], IL.InvoiceID, C.CustomerName
 
-go
-
--- есть проблемы с подзапросом. помесячно выводит, но без нарастающего итога
-
-Print(CHAR(13) + 'Query with Subquery')
+print(CHAR(13) + 'Query with Subquery')
 
 select 
 	IL.InvoiceID,
@@ -60,15 +54,13 @@ select
 		join [Sales].[Invoices] as IInner
 		on IInner.InvoiceID = ILInner.InvoiceID
 		where IInner.InvoiceDate  >= '2015-01-01' 
-		and IInner.InvoiceDate <= EOMONTH(I.InvoiceDate)
+		and IInner.InvoiceDate <= eomonth(I.InvoiceDate)
 		) as [Sale Amount by Month]
 from [Sales].[InvoiceLines] as IL
-join [Sales].[Invoices] as I
-on I.InvoiceID = IL.InvoiceID
-join [Sales].[Customers] as C
-on C.CustomerID = I.CustomerID
+join [Sales].[Invoices] as I on I.InvoiceID = IL.InvoiceID
+join [Sales].[Customers] as C on C.CustomerID = I.CustomerID
 where I.InvoiceDate  >= '2015-01-01'
-order by I.InvoiceDate, IL.Quantity*IL.UnitPrice, IL.InvoiceID, C.CustomerName
+order by I.InvoiceDate, [Sale Amount], IL.InvoiceID, C.CustomerName
 
 
 set statistics time off;
@@ -89,13 +81,10 @@ set statistics time off;
 		sum(InvLine.Quantity) over(partition by Item.StockItemID, month(Inv.InvoiceDate) order by InvLine.Quantity desc) as [Sale Quantity],
 		row_number() over(partition by Item.StockItemID, month(Inv.InvoiceDate) order by InvLine.Quantity) as Quantity
 	from [Sales].[InvoiceLines] as InvLine
-	join [Warehouse].[StockItems] as Item
-	on Item.StockItemID = InvLine.StockItemID
-	join [Sales].[Invoices] as Inv
-	on Inv.InvoiceID = InvLine.InvoiceID
+	join [Warehouse].[StockItems] as Item on Item.StockItemID = InvLine.StockItemID
+	join [Sales].[Invoices] as Inv on Inv.InvoiceID = InvLine.InvoiceID
 	where year(Inv.InvoiceDate) = '2016'
-	group by Item.StockItemID, year(Inv.InvoiceDate),
-		month(Inv.InvoiceDate), InvLine.Quantity
+	group by Item.StockItemID, year(Inv.InvoiceDate), month(Inv.InvoiceDate), InvLine.Quantity
 )
 select *
 from MonthQuantityCTE
@@ -120,13 +109,13 @@ select
 	It.StockItemName,
 	It.Brand,
 	It.UnitPrice,
-	ROW_NUMBER() over(partition by left(It.StockItemName, 1) order by It.StockItemName) as [Item Name Alphabetically],
+	row_number() over(partition by left(It.StockItemName, 1) order by It.StockItemName) as [Item Name Alphabetically],
 	count(*) over() as [Item Count],
-	count(*) over(partition by left(It.StockItemName, 1)) as [Item Count Alphabetically]
-	,LEAD(It.StockItemID) over(order by It.StockItemName) as [Next ID]
-	,LAG(It.StockItemID) over(order by It.StockItemName) as [Prev ID]
-	,LAG(It.StockItemName, 2, 'No items') over(order by It.StockItemName)  as [Prev 2 Name]
-	,NTILE(30) over(order by It.StockItemName) as [Grouped Weight Per Unit]
+	count(*) over(partition by left(It.StockItemName, 1)) as [Item Count Alphabetically],
+	lead(It.StockItemID) over(order by It.StockItemName) as [Next ID],
+	lag(It.StockItemID) over(order by It.StockItemName) as [Prev ID],
+	lag(It.StockItemName, 2, 'No items') over(order by It.StockItemName)  as [Prev 2 Name],
+	ntile(30) over(order by It.StockItemName) as [Grouped Weight Per Unit]
 from [Warehouse].[StockItems] as It
 
 
@@ -136,21 +125,18 @@ from [Warehouse].[StockItems] as It
 select
 	distinct(P.PersonID),
 	P.FullName,
-	LAST_VALUE(C.CustomerID) over(partition by P.PersonID 
+	last_value(C.CustomerID) over(partition by P.PersonID 
 		order by I.InvoiceDate rows between unbounded preceding and unbounded following) as LastCustomerID,
-	LAST_VALUE(C.CustomerName) over(partition by P.PersonID 
+	last_value(C.CustomerName) over(partition by P.PersonID 
 		order by I.InvoiceDate rows between unbounded preceding and unbounded following) as LastCustomerName,
-	LAST_VALUE(I.InvoiceDate) over(partition by P.PersonID 
+	last_value(I.InvoiceDate) over(partition by P.PersonID 
 		order by I.InvoiceDate rows between unbounded preceding and unbounded following) as LastInvoiceDate,
-	LAST_VALUE(CT.TransactionAmount) over(partition by P.PersonID 
+	last_value(CT.TransactionAmount) over(partition by P.PersonID 
 		order by I.InvoiceDate rows between unbounded preceding and unbounded following) as LastTransactionAmount
 from [Application].[People] as P
-join [Sales].[Invoices] as I
-on I.SalespersonPersonID = P.PersonID
-join [Sales].[CustomerTransactions] as CT
-on CT.InvoiceID = I.InvoiceID
-join [Sales].[Customers] as C
-on C.CustomerID = CT.CustomerID
+join [Sales].[Invoices] as I on I.SalespersonPersonID = P.PersonID
+join [Sales].[CustomerTransactions] as CT on CT.InvoiceID = I.InvoiceID
+join [Sales].[Customers] as C on C.CustomerID = CT.CustomerID
 where P.IsEmployee = 1
 order by P.PersonID
 
@@ -168,14 +154,10 @@ order by P.PersonID
 		I.InvoiceDate,
 		row_number() over(partition by C.CustomerID order by SI.UnitPrice desc) as Number
 	from [Sales].[Customers] as C
-	join [Sales].[CustomerTransactions] as CT
-	on CT.CustomerID = C.CustomerID
-	join [Sales].[InvoiceLines] as IL
-	on IL.InvoiceID = CT.InvoiceID
-	join [Warehouse].[StockItems] as SI
-	on SI.StockItemID = IL.StockItemID
-	join [Sales].[Invoices] as I
-	on I.InvoiceID = IL.InvoiceID
+	join [Sales].[CustomerTransactions] as CT on CT.CustomerID = C.CustomerID
+	join [Sales].[InvoiceLines] as IL on IL.InvoiceID = CT.InvoiceID
+	join [Warehouse].[StockItems] as SI	on SI.StockItemID = IL.StockItemID
+	join [Sales].[Invoices] as I on I.InvoiceID = IL.InvoiceID
 )
 select *
 from CustomersItemsCTE as Cust
