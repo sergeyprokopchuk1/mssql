c215f678d93ecb8a20038cc2310215523a27d994
Prokopchuk Sergey <pro5erg.gkhk@gmail.com>
2019-03-19 11:18:48 +0200

    Lesson 2. The other tasks.

--- Lesson 2/HomeWork2.sql
+++ Lesson 2/HomeWork2.sql
@@ -12,7 +12,8 @@ go
 с ценой товара более 100$ либо количество единиц товара более 20. 
 Добавьте вариант этого запроса с постраничной выборкой пропустив первую 1000 и отобразив следующие 100 записей. 
 Соритровка должна быть по номеру квартала, трети года, дате продажи. 
-4. Заказы поставщикам, которые были исполнены за 2014й год с доставкой Road Freight или Post, добавьте название поставщика, имя контактного лица принимавшего заказ
+4. Заказы поставщикам, которые были исполнены за 2014й год с доставкой Road Freight или Post, 
+добавьте название поставщика, имя контактного лица принимавшего заказ
 5. 10 последних по дате продаж с именем клиента и именем сотрудника, который оформил заказ.
 6. Все ид и имена клиентов и их контактные телефоны, которые покупали товар Chocolate frogs 250g
 */
@@ -41,3 +42,53 @@ where ST.SupplierID is null
 Соритровка должна быть по номеру квартала, трети года, дате продажи. 
 */
 
+select O.OrderDate
+	,OL.PickingCompletedWhen
+	,datename(month, convert(char(8), O.OrderDate, 112)) as [Month Name]
+	,datepart(QUARTER, O.OrderDate) as [Quarter Number]
+	,(datepart(month, O.OrderDate)-1)/4 + 1 as [Third Number]
+from [Sales].[Orders] as O
+join [Sales].[OrderLines] as OL on OL.OrderID = O.OrderID
+where OL.PickingCompletedWhen is not null
+and (OL.UnitPrice > 100 or OL.Quantity > 20)
+order by [Quarter Number], [Third Number], O.OrderDate
+offset 1000 rows fetch next 100 rows only
+
+--4. Заказы поставщикам, которые были исполнены за 2014й год с доставкой Road Freight или Post, 
+--добавьте название поставщика, имя контактного лица принимавшего заказ
+
+select O.PurchaseOrderID
+	,S.SupplierID
+	,S.SupplierName
+	,P.FullName
+	,O.OrderDate
+from [Purchasing].[Suppliers] as S
+join [Purchasing].[PurchaseOrders] as O on O.SupplierID = S.SupplierID
+join [Application].[People] as P on O.ContactPersonID = P.PersonID
+where year(O.OrderDate) = 2014
+and (O.DeliveryMethodID = 1 or O.DeliveryMethodID = 7)
+order by O.PurchaseOrderID
+
+--5. 10 последних по дате продаж с именем клиента и именем сотрудника, который оформил заказ.
+
+select top(10) O.OrderDate
+	,O.OrderID
+	,SP.FullName
+	,C.CustomerName
+from [Sales].[Orders] as O
+join [Application].[People] as SP on SP.PersonID = O.SalespersonPersonID
+join [Sales].[Customers] as C on C.CustomerID = O.CustomerID
+where SP.IsSalesperson = 1
+order by O.OrderDate desc
+
+--6. Все ид и имена клиентов и их контактные телефоны, которые покупали товар Chocolate frogs 250g
+
+select
+	C.CustomerID
+	,C.CustomerName
+	,C.PhoneNumber
+from [Sales].[Customers] as C
+join [Sales].Orders as O on O.CustomerID = C.CustomerID
+join [Sales].[OrderLines] as OL on OL.OrderID = O.OrderID
+join [Warehouse].[StockItems] as I on I.StockItemID = OL.StockItemID
+where I.StockItemName = 'Chocolate frogs 250g'
